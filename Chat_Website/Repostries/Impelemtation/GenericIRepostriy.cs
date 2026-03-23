a5fd5b2c9294e8fef3ec08b4d79dbc1e2ba9eaf5
Jacqueline Saad <jacquelinesaad497@gmail.com>
2026-03-24 00:18:51 +0200

    Create Impelemtation and Interfaces Repostry to avoid using DbContext in any Controller

---
+++ Chat_Website/Repostries/Impelemtation/GenericIRepostriy.cs
@@ -0,0 +1,48 @@
+﻿namespace Chat_Website.Repostries.Impelemtation
+{
+    public class GenericIRepostriy<T> : IRepostriy<T> where T : class
+    {
+        private readonly ChatDbContext _chatDbContext;
+        private readonly DbSet<T> _dbSet;
+        public GenericIRepostriy(ChatDbContext chatDbContext)
+        {
+            _chatDbContext = chatDbContext;
+            _dbSet = _chatDbContext.Set<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public List<T> GetAllList()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T? GetByID(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void Save()
+        {
+            _chatDbContext.SaveChanges();
+        }
+    }
+}
---
+++ Chat_Website/Repostries/Interfaces/IRepostriy.cs
@@ -0,0 +1,10 @@
+﻿public interface IRepostriy<T>
+{
+    IQueryable<T> GetAll();
+    List<T> GetAllList();
+    T? GetByID(int id);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    void Save();
+}
